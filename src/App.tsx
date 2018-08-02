1bec4026739eee1d3573536c3b3553e703c0305f
dan <dhua.mobile@gmail.com>
2018-08-02 15:22:02 -0400

    add prettier and tslint

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,18 @@
  * @flow
  */
 
-import React from 'react';
-import { Component } from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+})
 
-type Props = {};
-export default class App extends Component<Props> {
+type Props = {}
+export default class App extends React.Component<Props> {
   render() {
     return (
       <View style={styles.container}>
@@ -26,7 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -47,4 +46,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
